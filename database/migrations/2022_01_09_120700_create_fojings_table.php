6e3b03148316f54e5a6c55b7629f04393040c82b
Michael <michael@MichaeldeMacBook-Pro.local>
2022-01-09 21:06:37 +0800

    first migrate

---
+++ database/migrations/2022_01_09_120700_create_fojings_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFojingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fojings', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('album_id')->constrained();
+            $table->string('name');
+            $table->string('pathName');
+            $table->string('url');
+            $table->integer('sort');
+            $table->string('ext');
+            $table->integer('playback');
+            $table->decimal('filesize', $precision = 8, $scale = 2);
+            $table->integer('type');
+            $table->string('jump_url');
+            $table->string('cover');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fojings');
+    }
+}
